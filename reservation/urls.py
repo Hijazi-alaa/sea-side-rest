11aab79ac07b9e139995089c3f27191967bd5e38
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-04-08 14:30:18 +0000

    Add delete booking functionality

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BookingList.as_view(), name='bookings'),
     path('edit/<booking_id>', views.edit_booking, name='edit'),
+    path('delete/<booking_id>', views.delete_booking, name='delete'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -49,4 +49,8 @@ def edit_booking(request, booking_id):
     }
     return render(request, 'update_booking.html', context)
 
-    
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('/')
